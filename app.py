3f686654010c91dd888163741a0fe05eedd3a8f6
Sahil Shaikh <sahil.shaikh24@aiml.sce.edu.in>
2025-09-19 20:31:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,5 +148,5 @@ def random_dog():
     except Exception as e:
         return jsonify({'error': str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
